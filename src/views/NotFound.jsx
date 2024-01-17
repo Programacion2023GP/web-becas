71b8d7174455d26ae61d1b08363978d6e1b86da4
Nestor <nestorpuentesin@gmail.com>
2024-01-17 12:16:30 -0600

    se agregaron las imagenes

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -2,6 +2,7 @@ import { Button, Container, Typography } from "@mui/material";
 import Icon404 from "../components/icons/Icon404";
 import { Link, useRouteError } from "react-router-dom";
 import { HomeTwoTone } from "@mui/icons-material";
+import ImgNotFound from "../assets/images/not-found.jpg";
 
 const NotFound = () => {
    const error = useRouteError();
@@ -11,43 +12,28 @@ const NotFound = () => {
 
    switch (error.status) {
       case 404:
-         errorText =
-            "¡La página que está buscando fue movida, eliminada, renombrada o podría no existir nunca!";
-         srcImg = "/src/assets/images/not-found.jpg";
+         errorText = "¡La página que está buscando fue movida, eliminada, renombrada o podría no existir nunca!";
+         srcImg = ImgNotFound;
          break;
       case 403:
          break;
 
       default:
-         errorText =
-            "¡La página que está buscando fue movida, eliminada, renombrada o podría no existir nunca!";
-         srcImg = "/src/assets/images/not-found.jpg";
+         errorText = "¡La página que está buscando fue movida, eliminada, renombrada o podría no existir nunca!";
+         srcImg = ImgNotFound;
          break;
    }
 
    return (
       <Container sx={{ textAlign: "center" }}>
          <img src={srcImg} width={"80%"} />
-         <Typography
-            variant="h1"
-            mt={3}
-            sx={{ fontWeight: "900" }}
-            textTransform={"uppercase"}
-         >
+         <Typography variant="h1" mt={3} sx={{ fontWeight: "900" }} textTransform={"uppercase"}>
             Algo está mal
          </Typography>
-         <Typography
-            variant="body1"
-            sx={{ width: "35%", textAlign: "center", mx: "auto", my: 3 }}
-         >
+         <Typography variant="body1" sx={{ width: "35%", textAlign: "center", mx: "auto", my: 3 }}>
             {errorText}
          </Typography>
-         <Button
-            variant="contained"
-            component={Link}
-            to="/"
-            startIcon={<HomeTwoTone />}
-         >
+         <Button variant="contained" component={Link} to="/" startIcon={<HomeTwoTone />}>
             Regresar al inicio
          </Button>
       </Container>
